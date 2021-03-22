d84210a8caa9521ae662fd8c4612189a28a29cff
KC Tanenui-Manukau <tanenuimanukauk9154@masseyhigh.school.nz>
2021-03-22 21:14:01 +0000

    changed showing to have fun if the user decides that they are having fun or they are not

--- 02_yes_no.py
+++ 02_yes_no.py
@@ -1,16 +1,40 @@
+#Functions go here..
 
-keep_going = ""
-while keep_going == "":
+def yes_no(question):
+  valid  = False
+  while not valid:
+    response = input (question).lower()
+
+    if response == "yes" or response == "y":
+      response = "yes"
+      return response
+
+    elif response == "no" or response == "n":
+      response = "no"
+      return response
+
+    else:
+      print("Please answer yes/no")
+
+# Main routine goes here...
+
+show_instructions = yes_no("Have you played the game before? ")
+print("You chose {}".format(show_instructions))
+print()
+having_fun = yes_no("Are you having fun? ")
+print("You said {} to having fun".format(having_fun))
+
+show_instructions = ""
+while show_instructions != "xxx":
 
   # .lower() makes things lowercase (not UPPERCASE)
   show_instructions = input("Have you played before? ").lower()
 
-  if show_instructions == "yes":
+  if show_instructions == "yes" or show_instructions == "y":
     print("program continues")
-  elif show_instructions == "no":
+  elif show_instructions == "no" or show_instructions == "n":
     print("display instructions")
   else: 
     print("please enter yes / no")
 
   print()
-
